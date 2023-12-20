5d6bbc1f86596c79383324c0a70527c12e5d9045
AuroreDsmith <aurore.desmis@gmail.com>
2023-12-20 14:57:32 +0100

    Style sur authentif, header et footer

--- src/app/components/root/app.component.ts
+++ src/app/components/root/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 import { ListChannelComponent } from '../list-channel/list-channel.component';
 import { AddChannelComponent } from '../add-channel/add-channel.component';
 import { AuthentificationComponent } from '../authentification/authentification.component';
@@ -10,7 +11,7 @@ import { UserService } from '../../services/user.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, ListChannelComponent, AddChannelComponent, AuthentificationComponent, CommonModule],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, ListChannelComponent, AddChannelComponent, AuthentificationComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
