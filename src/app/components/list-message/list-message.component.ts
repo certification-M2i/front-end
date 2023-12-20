4197dbbad9c4ac4505df11300e321f0c52e58521
Mickaël Seffar <mickael.seffar@gmail.com>
2023-12-20 12:31:18 +0100

    premier jet messages

--- src/app/components/list-message/list-message.component.ts
+++ src/app/components/list-message/list-message.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MessageService} from "../../services/message.service";
+import {UserService} from "../../services/user.service";
+import {NgForOf} from "@angular/common";
+import {ChannelService} from "../../services/channel.service";
 
 @Component({
   selector: 'app-list-message',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './list-message.component.html',
   styleUrl: './list-message.component.css'
 })
-export class ListMessageComponent {
+export class ListMessageComponent implements OnInit{
+
+  constructor(public channelService:ChannelService, public messageService:MessageService) {}
+
+  ngOnInit():void{
+    this.channelService.getMessageInChannel(this.channelService.getChannelId());
+  }
 
 }
