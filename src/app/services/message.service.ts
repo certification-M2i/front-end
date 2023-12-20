3889529a9c7d86f017ecdc707df2c5e151c34238
allan <allanduminy@protonmail.com>
2023-12-20 08:56:00 +0100

    suppression algo distance de leveinshtein

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { FetcherService } from './fetcher.service';
 import { MessageGet } from '../models/MessageGet.model';
 import { MessagePost } from '../models/MessagePost.model';
-import { calculateMatchWeight } from '../utils/utils';
 
 @Injectable({
   providedIn: 'root'
@@ -28,19 +27,5 @@ export class MessageService {
 
   postMessage(message: MessagePost) {
     return this.fetcher.postMessage(message);
-  }
-
-  searchMessages(searchTerm : string) {
-    this.getAllMessages();
-    
-    const searchTermLower = searchTerm.toLowerCase();
-    const results = this.messageList.map(message => ({
-        ...message,
-        weight: calculateMatchWeight(searchTermLower, message.content.toLowerCase())
-    }));
-
-    return results
-        .sort((a, b) => b.weight - a.weight)
-        .filter(item => item.weight > 0);
   }
 }
